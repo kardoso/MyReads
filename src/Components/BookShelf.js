fddcd3f249e7beca83f65e03050faa58182f3dc6
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-04 05:17:32 -0300

    feat: Create Book component

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Book from './Book'
+
 function BookShelf(props) {
   return (
     <div className="bookshelf">
@@ -10,35 +12,13 @@ function BookShelf(props) {
           <p>There's nothing to show here</p>
         ) : (
           <ol className="books-grid">
-            {props.books.map(book => (
-              <li key={book.title}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                      }}
-                    ></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
+            {props.books.map((book, id) => (
+              <li key={id}>
+                <Book
+                  title={book.title}
+                  authors={book.authors}
+                  image={book.imageLinks.smallThumbnail}
+                />
               </li>
             ))}
           </ol>
