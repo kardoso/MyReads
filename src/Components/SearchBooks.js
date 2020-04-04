eadebaaf26129699ce75077baa77d5a272066337
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-04 09:01:04 -0300

    feat: Dynamic search

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -6,30 +6,50 @@ import Book from './Book'
 class SearchBooks extends Component {
   constructor(props) {
     super(props)
+    this.books = this.props.books
     this.state = {
-      books: props.books
+      searchString: ''
     }
+
+    this.updateSearch = this.updateSearch.bind(this)
+  }
+
+  updateSearch(e) {
+    this.setState({
+      searchString: e.target.value
+    })
   }
 
   render() {
+    const books = this.books.filter(
+      book =>
+        book.title
+          .toLowerCase()
+          .includes(this.state.searchString.toLowerCase()) ||
+        book.authors
+          .map(author => {
+            return author
+          })
+          .join('')
+          .toLowerCase()
+          .includes(this.state.searchString.toLowerCase())
+    )
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/" />
           <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={this.updateSearch}
+            />
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.books.map((book, id) => (
+            {books.map(book => (
               <li key={book.id}>
                 <Book book={book} onUpdate={this.props.onUpdate} />
               </li>
